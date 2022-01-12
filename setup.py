1bbbb325e8fa40095223e82b45d555a21cd47b11
jkbgbr <jkbgbr@gmail.com>
2022-01-12 23:32:07 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pymntr',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='jkbgbr',
+    author_email='jkbgbr@gmail.com',
+    description='A tool to visualize ANSYS APDL .mntr file content'
+)
