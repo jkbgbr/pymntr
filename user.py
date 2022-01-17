20f667c922ae9d1f48ae7d0e53eef1c851ca72a4
jkbgbr <jkbgbr@gmail.com>
2022-01-17 18:03:33 +0100

    minor

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 import os
 from main import MNTR
 
-# files = MNTR.discover_mntr_files(os.path.abspath('D:\\ANSYS\\20.167.90 - Delle\\'), filter='x0_')
-files = MNTR.discover_mntr_files(os.path.abspath('D:\\Berechnungen\\20.167.90 - Delle\\'), filter='900x520x15_GMNIA')
+files = MNTR.discover_mntr_files(os.path.abspath('D:\\ANSYS\\20.167.90 - Delle\\'), filter='_GMNIA')
+# files = MNTR.discover_mntr_files(os.path.abspath('D:\\Berechnungen\\20.167.90 - Delle\\'), filter='900x520x15_GMNIA')
 # monitor = MNTR(files[-1])
 
 # monitor.plot()
