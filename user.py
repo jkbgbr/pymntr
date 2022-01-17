925773a2482728fdfcf7b81e57c77fdc5e007bbf
JakabG <jkbgbr@gmail.com>
2022-01-17 09:48:08 +0100

    moved user-side calls to a separate module

---
+++ user.py
@@ -0,0 +1,12 @@
+import os
+from main import MNTR
+
+# files = MNTR.discover_mntr_files(os.path.abspath('D:\\ANSYS\\20.167.90 - Delle\\'), filter='x0_')
+files = MNTR.discover_mntr_files(os.path.abspath('D:\\Berechnungen\\20.167.90 - Delle\\'), filter='900x520x15_GMNIA')
+# monitor = MNTR(files[-1])
+
+# monitor.plot()
+for file in files:
+    monitor = MNTR(file)
+    print(monitor.jobname, '{:.4f}'.format(max(monitor.data['variable2'])))
+    monitor.plot(vertical=('time',), horizontal='variable2')
