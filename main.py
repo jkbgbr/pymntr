db3c1ecfaa180e64983a7042221688562100cc4e
JakabG <jkbgbr@gmail.com>
2022-01-17 09:22:27 +0100

    minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 import re
 import time
 import matplotlib.pyplot as plt
@@ -146,10 +147,12 @@ class MNTR:
 
 
 if __name__ == '__main__':
-    files = MNTR.discover_mntr_files(os.path.abspath('D:\\ANSYS\\20.167.90 - Delle\\'), filter='x0_')
+    # files = MNTR.discover_mntr_files(os.path.abspath('D:\\ANSYS\\20.167.90 - Delle\\'), filter='x0_')
+    files = MNTR.discover_mntr_files(os.path.abspath('D:\\Berechnungen\\20.167.90 - Delle\\'), filter='900x520x15_GMNIA')
     # monitor = MNTR(files[-1])
+
     # monitor.plot()
     for file in files:
         monitor = MNTR(file)
-        print(monitor.jobname, '{:.4f}'.format(max(monitor.data['time'])))
-        monitor.plot()
+        print(monitor.jobname, '{:.4f}'.format(max(monitor.data['variable2'])))
+        monitor.plot(vertical=('time',), horizontal='variable2')
